805b42c8b5ea6e0ed087e60263cc85d8f809fecd
pangkup <peerada262007@gmail.com>
2024-05-02 14:47:27 +0700

    grade

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
+        if ( number <= 49 ){
+            System.out.println("grade f" + number);
+        }
+        else if ( number > 49 && number <= 60) {
+            System.out.println("grae d " + number);
 
+        }
+        else if ( number > 60 && number <= 70) {
+            System.out.println("grae c+ " + number);
+
+        }
+        else if ( number > 70 && number <= 80) {
+            System.out.println("grae b+ " + number);
+
+        }
+        else if ( number > 80 && number <= 100) {
+            System.out.println("grae a+ " + number);
+
+        }
     }
 }
